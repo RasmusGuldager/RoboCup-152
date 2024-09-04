7fcc4ec39e4ac798d4e26413bf84e15aea708a45
gjellerhjort <mads.gjellerod@gmail.com>
2024-09-04 12:57:10 +0200

    ny test kode

---
+++ README.md
@@ -0,0 +1,6 @@
+# links
+
+## PID kontrol
+https://thecodingfun.com/2020/06/16/lego-mindstorms-ev3-pid-line-follower-code-by-using-micropython-2-0/
+
+##
--- test.py
+++ test.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import main
 
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
 
-print(main.checkColor())
+def follow_line_motors()
+    # venstre motor
+    left_motor = Motor(Port.A,Direction.ClOCKWISE)
+    # højre motor
+    right_motor = Motor(Port.B,Direction.ClOCKWISE)
+    # farve sensor
+    line_sensor = ColorSensor(Port.S3)
+
+    # sort og hvis værdier til calbriing 
+    BLACK = 3
+    WHITE = 62
+    # denne værdi er hvornår den er hvid eller sort
+    threshold = (BLACK+WHITE) / 2
+    # hastighed på Motor
+    DRIVE_SPEED = 100
+    # Hvor meget kræft den skal dreje med
+    PROPORTIONAL_GAIN=1.4
+
+while True:
+    # hvor meget den er væk fra stregen
+    error = line_sensor.reflection()-threshold
+    # hvor meget kræft den skal dreje med bliver større jo længere væk den er fra starten
+    turn_rate = PROPORTIONAL_GAIN * error
+    # drejer motor
+    left_motor(int(DRIVE_SPEED+turn_rate))
+    right_motor(int(DRIVE_SPEED-turn_rate))
+    wait(10)
