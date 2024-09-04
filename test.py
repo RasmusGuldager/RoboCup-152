a63f5f02a24c492d8a25aff06f09a21f0945fe41
gjellerhjort <mads.gjellerod@gmail.com>
2024-09-04 14:19:08 +0200

    ny test ting

--- test.py
+++ test.py
@@ -6,18 +6,24 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# Initialize the EV3 Brick.
-ev3 = EV3Brick()
+
+
+def color_calibration()
+    if (touch_sensor.pressed()):
+        BLACK = line_sensor.reflection()
+        if BLACK == 0:
+            return 0
+            
+    if (touch_sensor.pressed()):
+        WHITE = line_sensor.reflection()
+        if WHITE == 0:
+            return 0
+        else:
+            return 1
 
 def follow_line_motors()
-    # venstre motor
-    left_motor = Motor(Port.A,Direction.ClOCKWISE)
-    # højre motor
-    right_motor = Motor(Port.B,Direction.ClOCKWISE)
-    # farve sensor
-    line_sensor = ColorSensor(Port.S3)
 
-    # sort og hvis værdier til calbriing 
+    # sort og hvis værdier til kalibrering 
     BLACK = 3
     WHITE = 62
     # denne værdi er hvornår den er hvid eller sort
@@ -27,12 +33,32 @@ def follow_line_motors()
     # Hvor meget kræft den skal dreje med
     PROPORTIONAL_GAIN=1.4
 
-while True:
-    # hvor meget den er væk fra stregen
-    error = line_sensor.reflection()-threshold
-    # hvor meget kræft den skal dreje med bliver større jo længere væk den er fra starten
-    turn_rate = PROPORTIONAL_GAIN * error
-    # drejer motor
-    left_motor(int(DRIVE_SPEED+turn_rate))
-    right_motor(int(DRIVE_SPEED-turn_rate))
-    wait(10)
+def main()
+    while menu==1:
+        if (touch_sensor.pressed()):
+            status = color_calibration()
+
+
+    while status==1:
+        # hvor meget den er væk fra stregen
+        error = line_sensor.reflection()-threshold
+        # hvor meget kræft den skal dreje med bliver større jo længere væk den er fra starten
+        turn_rate = PROPORTIONAL_GAIN * error
+        # drejer motor
+        left_motor(int(DRIVE_SPEED+turn_rate))
+        right_motor(int(DRIVE_SPEED-turn_rate))
+        wait(10)
+
+if __name__ == "__main__":
+    print("start")
+    sound = SoundFile()
+    touch_sensor = TouchSensor(Port.S1)
+    # venstre motor
+    left_motor = Motor(Port.A,Direction.ClOCKWISE)
+    # højre motor
+    right_motor = Motor(Port.B,Direction.ClOCKWISE)
+    # farve sensor
+    line_sensor = ColorSensor(Port.S3)
+    status = 0
+    menu = 1
+    main()
